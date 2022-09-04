26458b412e2d7cc43f16f4c6c309fd2dbda86a94
Darren_Hsu <bthd0404@gmail.com>
2022-09-05 00:37:19 +0800

    add: 第一版完成

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,72 @@
-console.log('Hello!');
-
-$(document).ready(() => {
-  console.log('HesSchool Hello!');
+$('#burger').on('click', () =>{
+  $('#burger').toggleClass('open');
+  $('#menu').slideToggle();
 });
+$('#filterOpen').on('click', () =>{
+  $('#filterList').fadeToggle();
+  $('#filterModal').fadeToggle();
+})
+$('#filterClose').on('click', () =>{
+  $('#filterList').fadeToggle();
+  $('#filterModal').fadeToggle();
+})
+
+let mySwiper = 
+new Swiper('.swiper.PC', {
+  autoplay: {
+    disableOnInteraction: false,
+  },
+  loop: true,
+  slidesPerView: 3,
+  effect: 'coverflow',  // effect 不支援不同斷點改變屬性，改用兩個 swiper 來做
+  coverflowEffect: {
+    scale: .9,
+    rotate: 0,
+    depth: 0,
+    modifier: 1,
+    slideShadows: false,
+  },
+  pagination: {
+    el: '.swiper-pagination.PC',
+    clickable: true,
+  },
+})
+new Swiper('.swiper.mobile', {
+  autoplay: {
+    disableOnInteraction: false,
+  },
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination.mobile',
+    clickable: true,
+  },
+})
+
+let introduceSwaiper = new Swiper('.swiper-introduce', {
+  autoplay: {
+    disableOnInteraction: false,
+  },  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+})
+
+let msnry = new Masonry( '.grid-fail', {
+  columnWidth: '.grid-item',
+  gutter: 24,
+  itemSelector: '.grid-item',
+  percentPosition: true
+});
+
+const rankingCollapse = document.querySelector('#collapse');
+rankingCollapse.addEventListener('click', (e) =>{
+  if(e.target.nodeName !== 'A') return;
+  let list = e.target.parentNode.parentNode.lastElementChild;
+  e.target.classList.toggle('fa-plus')
+  e.target.classList.toggle('fa-minus')
+  e.target.classList.toggle('after:border-b-black')
+  e.target.classList.toggle('after:border-b-orange')
+  list.classList.toggle('max-h-0')
+  list.classList.toggle('max-h-96')
+  list.classList.toggle('border')
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,66 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ["./app/**/*.{html,ejs}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '12px',
+      },
+      screens: {
+        sm: '576px',
+        md: '720px',
+        lg: '992px',
+        xl: '1320px',
+      },
+    },
+    fontFamily: {
+      'sans': ['"Squada One"', '"Noto Sans TC"', ...defaultTheme.fontFamily.sans],
+      'noto': '"Noto Sans TC"',
+      'paytone': '"Paytone One"'
+    },
+    extend: {
+      fontSize: {
+        'sm': '14px',
+        'base': '16px',
+        'lg': '18px',
+        'xl': '20px',
+        '2xl': '24px',
+        '3xl': '32px',
+        '4xl': '36px',
+        '5xl': '48px',
+        '6xl': '80px',
+      },
+      colors: {
+        gray: {
+          DEFAULT: '#F0F0F0',
+          '100': '#00000029',
+          '200': '#808080'
+        },
+        orange: {
+          DEFAULT: '#E6553B'
+        },
+        green: {
+          DEFAULT: '#53C139'
+        }
+      },
+      borderWidth: {
+        '10': '10px',
+        '16': '16px',
+        '24': '24px',
+        '32': '32px',
+      },
+      borderColor: {
+        DEFAULT: '#000000'
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
+    require('tailwindcss-writing-mode')({
+      variants: ['responsive', 'hover']
+  }),
   ],
 }
